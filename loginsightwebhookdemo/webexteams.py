7b784ae759c93f99d4158e1962195d86e856634b
Brad Calvert <brad.calvert@hcahealthcare.com>
2018-09-17 09:45:49 -0500

    tag 1.0-rc1

--- loginsightwebhookdemo/webexteams.py
+++ loginsightwebhookdemo/webexteams.py
@@ -4,15 +4,22 @@ from loginsightwebhookdemo import app, parse, callapi
 from flask import request, json
 import logging
 from datetime import datetime
-
+import pytz
 
 __author__ = "Jason Cantrell"
+__contributions__ = "Brad Calvert"
 __license__ = "Apache v2"
 __email__ = "cantrell.jm@gmail.com"
+__email__ = "bcalvert@gmail.com"
 __version__ = "1.0"
 
+# Due to sites in multiple time zones, our vROps is set to UTC. Adjust this to your local timezone so the alerts post accordingly.
+timezone = pytz.timezone('America/Chicago')
 
-# Spark/Webex Teams incoming webhook URL. For more information see https://developer.webex.com/webhooks-explained.html
+# Spark/Webex Teams incoming webhook URL. For more information see:
+# https://developer.webex.com/webhooks-explained.html
+# https://apphub.webex.com/integrations/incoming-webhooks-cisco-systems
+#No usually no need to edit this unless you have a special case
 WXTEAMS = ''
 
 @app.route("/endpoint/wxteams", methods=['POST'])
@@ -34,8 +41,12 @@ def wxteams(HOOKID=None,RESOURCEID=None):
     a = parse(request)
 
     try:
+        # Webex Teams does not support message attachments in the same way that MS Teams or Slack do,
+        # so we chose to filter down the alerts to some critical relevant information. This is just a fraction of the fields that are avaiable
         if ('alertId' in a):
-            alertTime = datetime.fromtimestamp(int(a['startDate'] / 1000.0)).strftime('%Y-%m-%d %H:%M:%S')
+            d = datetime.fromtimestamp(int(a['startDate'] / 1000.0)).replace(tzinfo=timezone)
+            alertTime = d.strftime('%Y-%m-%d %H:%M:%S') + timezone
+            
             message = 'Resource Name: {resourceName}\nAlert Name: {alertName}\nTimestamp: {alertTime}\nStatus: {alertStatus}\nCriticality: {alertCriticality}'.format(
                 resourceName=a['resourceName'],
                 alertName=a['AlertName'],
